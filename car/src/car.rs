30b20b6c329857a015fcea08af8cc0522ea83b58
Ali Khakpouri <ali.khakpouri@gmail.com>
2024-08-06 11:54:55 -0400

    completed the car exercise

--- car/src/car.rs
+++ car/src/car.rs
@@ -1,3 +1,5 @@
+use core::fmt;
+
 pub struct Car {
     pub color: String,
     pub transmission: Transmission,
@@ -6,8 +8,27 @@ pub struct Car {
 }
 
 #[derive(PartialEq, Debug)]
-enum Transmission {
+pub enum Transmission {
     Manual,
     SemiAuto,
     Automatic,
+}
+
+impl fmt::Display for Transmission {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Transmission::Manual => write!(f, "manual"),
+            Transmission::SemiAuto => write!(f, "semi-auto"),
+            Transmission::Automatic => write!(f, "auto"),
+        }
+    }
+}
+
+pub fn car_factory(color: String, transmission: Transmission, is_convirtible: bool) -> Car {
+    Car {
+        color: color,
+        convertible: is_convirtible,
+        mileage: 5000,
+        transmission: transmission,
+    }
 }
--- car/src/main.rs
+++ car/src/main.rs
@@ -1,4 +1,28 @@
-fn main() {
-    println!("Hello, world!");
-}
+use car::car_factory;
 
+mod car;
+
+fn main() {
+    let mut car = car_factory(String::from("Blue"), car::Transmission::Automatic, true);
+
+    println!(
+        "{} car is convertible {} has {} miles while driving a {} transmition",
+        car.color, car.convertible, car.mileage, car.transmission
+    );
+    car = car_factory(String::from("Red"), car::Transmission::SemiAuto, false);
+    println!(
+        "{} car is convertible {} has {} miles while driving a {} transmition",
+        car.color, car.convertible, car.mileage, car.transmission
+    );
+    car = car_factory(String::from("Yellow"), car::Transmission::SemiAuto, false);
+    println!(
+        "{} car is convertible {} has {} miles while driving a {} transmition",
+        car.color, car.convertible, car.mileage, car.transmission
+    );
+
+    car = car_factory(String::from("Silver"), car::Transmission::Manual, false);
+    println!(
+        "{} car is convertible {} has {} miles while driving a {} transmition",
+        car.color, car.convertible, car.mileage, car.transmission
+    );
+}
